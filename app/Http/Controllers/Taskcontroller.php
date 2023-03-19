6dd7888d15fd5bb1efcdef3e148cbc4ccd8f9a70
gaku25 <kanoya2_sgm@yahoo.co.jp>
2023-03-19 13:33:49 +0900

    commit

---
+++ app/Http/Controllers/Taskcontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Taskcontroller extends Controller
+{
+    public function index($text = "デフォルト")
+  {
+    return $text;
+  }
+
+public function add()
+  {
+    return $text;
+  }
+
+public function upd()
+  {
+    return $text;
+  }
+  
+public function del()
+  {
+    return $text;
+  }  
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="app.css">
+  <link rel="stylesheet" href="reset.css">
+  <title>ToDo-app</title>
+</head>
+
+<body>
+  <h1>{{$content}}</h1>
+</body>
+
+</html>
